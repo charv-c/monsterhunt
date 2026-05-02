aa1a5d57de141c63a306323e58729b14650b2739
wdgem <13317252+wdgem@user.noreply.gitee.com>
2026-05-03 00:22:58 +0800

    增加了敌人与玩家保持一定距离并且会沿着玩家为圆心运动的行动节点，未完成全部测试，镜头抖动严重，数值未完善

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public Transform playerTransform;
     private BehaviorTree.Node root;
+    NavMeshAgent agent;
     void Start()
     {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
         root = new BehaviorTree.Selector(new List<BehaviorTree.Node>
         {
-            new BehaviorTree.Selector(new List<BehaviorTree.Node>
+            new BehaviorTree.Sequence(new List<BehaviorTree.Node>
             {
-                new BehaviorTree.ObservePlayer(),
-                new BehaviorTree.MeleeAttackPlayer()
+                new BehaviorTree.ObservePlayer(transform, playerTransform, agent),
             }),
             new BehaviorTree.Sequence(new List<BehaviorTree.Node>
             {
-                new BehaviorTree.ObservePlayer(),
+                new BehaviorTree.ObservePlayer(transform, playerTransform, agent),
                 new BehaviorTree.RangedAttackPlayer()
             }),
             new BehaviorTree.SetTrap()
         });
+        
     }
 
     void Update()
